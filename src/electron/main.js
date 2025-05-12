5d408830dd32f8aeb17b49515a92693add7b9009
Weslley Batista <weslleyvitorbatista@gmail.com>
2025-05-12 11:06:22 -0300

    Criada estrutura incial.

---
+++ src/electron/main.js
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+  const iconPath = app.isPackaged
+    ? join(process.resourcesPath, "./build/devnotes-ico.png")
+    : join(__dirname, "../../build/devnotes-ico.png");
+
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    icon: iconPath,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(join(__dirname, "../../dist/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
